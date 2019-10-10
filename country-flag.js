b950ccbbe64e474f206d0026f346f277c3fcb70f
shafi13feb <shafi.khan@pfizer.com>
2019-10-10 15:51:34 +0530

    Added country-flag JSON file.

---
+++ country-flag.js
@@ -0,0 +1,764 @@
+[
+  {
+    machineName: "ar",
+    label: "Argentina",
+    languages: [
+      {
+        label: "Español",
+        machineName: "es",
+        shortName: "Es",
+        redirectUrl: "#.ar"
+      }
+    ]
+  },
+  {
+    machineName: "au",
+    label: "Australia",
+    languages: [
+      {
+        label: "English",
+        shortName: "En",
+        machineName: "en",
+        redirectUrl: "#.au"
+      }
+    ]
+  },
+  {
+    machineName: "at",
+    label: "Austria",
+    languages: [
+      {
+        label: "Deutsch",
+        shortName: "De",
+        machineName: "de",
+        redirectUrl: "#.at"
+      }
+    ]
+  },
+  {
+    machineName: "by",
+    label: "Belarus",
+    languages: [
+      {
+        label: "Belarusian",
+        shortName: "By",
+        machineName: "by",
+        redirectUrl: "#.by"
+      },
+      {
+        label: "English",
+        shortName: "En",
+        machineName: "en",
+        redirectUrl: "#.by/en"
+      }
+    ]
+  },
+  {
+    machineName: "be",
+    label: "Belgium",
+    languages: [
+      {
+        label: "English",
+        shortName: "En",
+        machineName: "en",
+        redirectUrl: "#.be"
+      },
+      {
+        label: "Nederlands",
+        shortName: "Nl",
+        machineName: "nl",
+        redirectUrl: "#.be/nl-be"
+      },
+      {
+        label: "Français",
+        shortName: "Fr",
+        machineName: "fr",
+        redirectUrl: "#.be/fr-be"
+      }
+    ]
+  },
+  {
+    machineName: "br",
+    label: "Brazil",
+    languages: [
+      {
+        label: "Português",
+        shortName: "Pr",
+        machineName: "pr",
+        redirectUrl: "#.br"
+      }
+    ]
+  },
+  {
+    machineName: "bg",
+    label: "Bulgaria",
+    languages: [
+      {
+        label: "Български",
+        shortName: "Bg",
+        machineName: "bg",
+        redirectUrl: "#.bg"
+      }
+    ]
+  },
+  {
+    machineName: "ca",
+    label: "Canada",
+    languages: [
+      {
+        label: "English",
+        shortName: "En",
+        machineName: "en",
+        redirectUrl: "#.ca"
+      },
+      {
+        label: "Français",
+        shortName: "Fr",
+        machineName: "fr",
+        redirectUrl: "#.ca/fr"
+      }
+    ]
+  },
+  {
+    machineName: "cn",
+    label: "China",
+    languages: [
+      {
+        label: "ä¸­æ–‡",
+        shortName: "Cn",
+        machineName: "cn",
+        redirectUrl: "#.cn"
+      },
+      {
+        label: "English",
+        shortName: "En",
+        machineName: "en",
+        redirectUrl: "#.cn/index_en.aspx"
+      }
+    ]
+  },
+  {
+    machineName: "co",
+    label: "Colombia",
+    languages: [
+      {
+        label: "Español",
+        shortName: "Es",
+        machineName: "es",
+        redirectUrl: "#.co"
+      }
+    ]
+  },
+  {
+    machineName: "hr",
+    label: "Croatia",
+    languages: [
+      {
+        label: "Croatian",
+        shortName: "Hr",
+        machineName: "hr",
+        redirectUrl: "#.hr"
+      }
+    ]
+  },
+  {
+    machineName: "cz",
+    label: "Czech Republic",
+    languages: [
+      {
+        label: "Čeština",
+        shortName: "Cz",
+        machineName: "cz",
+        redirectUrl: "#.cz"
+      }
+    ]
+  },
+  {
+    machineName: "dk",
+    label: "Denmark",
+    languages: [
+      {
+        label: "Dansk",
+        shortName: "Dk",
+        machineName: "dk",
+        redirectUrl: "#.dk"
+      }
+    ]
+  },
+  {
+    machineName: "ec",
+    label: "Ecuador",
+    languages: [
+      {
+        label: "Español",
+        shortName: "Ec",
+        machineName: "ec",
+        redirectUrl: "#.ec"
+      }
+    ]
+  },
+  {
+    machineName: "et",
+    label: "Estonia",
+    languages: [
+      {
+        label: "Eesti",
+        shortName: "Ee",
+        machineName: "et",
+        redirectUrl: "#.ee"
+      },
+      {
+        label: "English",
+        shortName: "En",
+        machineName: "en",
+        redirectUrl: "#.ee/en"
+      }
+    ]
+  },
+  {
+    machineName: "fi",
+    label: "Finland",
+    languages: [
+      {
+        label: "Suomalainen",
+        shortName: "Fi",
+        machineName: "fi",
+        redirectUrl: "#.fi"
+      }
+    ]
+  },
+  {
+    machineName: "fr",
+    label: "France",
+    languages: [
+      {
+        label: "Français",
+        shortName: "Fr",
+        machineName: "fr",
+        redirectUrl: "#.fr"
+      }
+    ]
+  },
+  {
+    machineName: "de",
+    label: "Germany",
+    languages: [
+      {
+        label: "Deutsch",
+        shortName: "De",
+        machineName: "de",
+        redirectUrl: "#.de"
+      }
+    ]
+  },
+  {
+    machineName: "gr",
+    label: "Greece",
+    languages: [
+      {
+        label: "ελληνικά",
+        shortName: "Gr",
+        machineName: "gr",
+        redirectUrl: "#.gr"
+      }
+    ]
+  },
+  {
+    machineName: "hk",
+    label: "Hong Kong",
+    languages: [
+      {
+        label: "繁體中文",
+        shortName: "Hk",
+        machineName: "hk",
+        redirectUrl: "#.hk"
+      },
+      {
+        label: "English",
+        shortName: "En",
+        machineName: "en",
+        redirectUrl: "#.hk/en"
+      }
+    ]
+  },
+  {
+    machineName: "hu",
+    label: "Hungary",
+    languages: [
+      {
+        label: "Magyar",
+        shortName: "Hu",
+        machineName: "hu",
+        redirectUrl: "#.hu"
+      }
+    ]
+  },
+  {
+    machineName: "india",
+    label: "India",
+    languages: [
+      {
+        label: "English",
+        shortName: "En",
+        machineName: "en",
+        redirectUrl: "#"
+      }
+    ]
+  },
+  {
+    machineName: "Indonesia",
+    label: "Indonesia",
+    languages: [
+      {
+        label: "Indonesian",
+        shortName: "In",
+        machineName: "in",
+        redirectUrl: "#peduli"
+      }
+    ]
+  },
+  {
+    machineName: "ie",
+    label: "Ireland",
+    languages: [
+      {
+        label: "English",
+        shortName: "En",
+        machineName: "en",
+        redirectUrl: "#"
+      }
+    ]
+  },
+  {
+    machineName: "il",
+    label: "Israel",
+    languages: [
+      {
+        label: "עברית",
+        shortName: "Il",
+        machineName: "il",
+        redirectUrl: "#.co.il"
+      }
+    ]
+  },
+  {
+    machineName: "it",
+    label: "Italy",
+    languages: [
+      {
+        label: "Italiano",
+        shortName: "It",
+        machineName: "it",
+        redirectUrl: "#.it"
+      }
+    ]
+  },
+  {
+    machineName: "jp",
+    label: "Japan",
+    languages: [
+      {
+        label: "日本の",
+        shortName: "Jp",
+        machineName: "jp",
+        redirectUrl: "#"
+      }
+    ]
+  },
+  {
+    machineName: "kr",
+    label: "Korea",
+    languages: [
+      {
+        label: "한국의",
+        shortName: "Kr",
+        machineName: "kr",
+        redirectUrl: "#.co.kr"
+      }
+    ]
+  },
+  {
+    machineName: "lv",
+    label: "Latvia",
+    languages: [
+      {
+        label: "Latvian",
+        shortName: "Lv",
+        machineName: "lv",
+        redirectUrl: "#.lv"
+      },
+      {
+        label: "English",
+        shortName: "En",
+        machineName: "en",
+        redirectUrl: "#.lv/en"
+      }
+    ]
+  },
+  {
+    machineName: "lt",
+    label: "Lithuania",
+    languages: [
+      {
+        label: "Lithuanian",
+        shortName: "Lt",
+        machineName: "lt",
+        redirectUrl: "#disclosure.lt"
+      },
+      {
+        label: "English",
+        shortName: "En",
+        machineName: "en",
+        redirectUrl: "#disclosure.lt/en"
+      }
+    ]
+  },
+  {
+    machineName: "my",
+    label: "Malaysia",
+    languages: [
+      {
+        label: "English",
+        shortName: "En",
+        machineName: "en",
+        redirectUrl: "#.my"
+      }
+    ]
+  },
+  {
+    machineName: "mx",
+    label: "Mexico",
+    languages: [
+      {
+        label: "Español",
+        shortName: "Es",
+        machineName: "es",
+        redirectUrl: "#.mx"
+      }
+    ]
+  },
+  {
+    machineName: "nl",
+    label: "Netherlands",
+    languages: [
+      {
+        label: "Nederlands",
+        shortName: "Nl",
+        machineName: "nl",
+        redirectUrl: "#.nl"
+      }
+    ]
+  },
+  {
+    machineName: "nz",
+    label: "New Zealand",
+    languages: [
+      {
+        label: "English",
+        shortName: "En",
+        machineName: "en",
+        redirectUrl: "#.co.nz"
+      }
+    ]
+  },
+  {
+    machineName: "no",
+    label: "Norway",
+    languages: [
+      {
+        label: "Norsk",
+        shortName: "No",
+        machineName: "no",
+        redirectUrl: "#.no"
+      }
+    ]
+  },
+  {
+    machineName: "pk",
+    label: "Pakistan",
+    languages: [
+      {
+        label: "English",
+        shortName: "En",
+        machineName: "en",
+        redirectUrl: "#.pk"
+      }
+    ]
+  },
+  {
+    machineName: "pe",
+    label: "Peru",
+    languages: [
+      {
+        label: "Español",
+        shortName: "Es",
+        machineName: "es",
+        redirectUrl: "#.pe"
+      }
+    ]
+  },
+  {
+    machineName: "ph",
+    label: "Philippines",
+    languages: [
+      {
+        label: "English",
+        shortName: "En",
+        machineName: "en",
+        redirectUrl: "#.ph"
+      }
+    ]
+  },
+  {
+    machineName: "pl",
+    label: "Poland",
+    languages: [
+      {
+        label: "Polski",
+        shortName: "Pl",
+        machineName: "pl",
+        redirectUrl: "#.pl"
+      }
+    ]
+  },
+  {
+    machineName: "pt",
+    label: "Portugal",
+    languages: [
+      {
+        label: "Português",
+        shortName: "Pt",
+        machineName: "pt",
+        redirectUrl: "#.pt"
+      }
+    ]
+  },
+  {
+    machineName: "ru",
+    label: "Russia",
+    languages: [
+      {
+        label: "Русский",
+        shortName: "Ru",
+        machineName: "ru",
+        redirectUrl: "#.ru"
+      }
+    ]
+  },
+  {
+    machineName: "rs",
+    label: "Serbia",
+    languages: [
+      {
+        label: "Serbian",
+        shortName: "Rs",
+        machineName: "rs",
+        redirectUrl: "#.rs"
+      }
+    ]
+  },
+  {
+    machineName: "sg",
+    label: "Singapore",
+    languages: [
+      {
+        label: "English",
+        shortName: "En",
+        machineName: "en",
+        redirectUrl: "#.sg"
+      }
+    ]
+  },
+  {
+    machineName: "si",
+    label: "Slovenia",
+    languages: [
+      {
+        label: "Slovenian",
+        shortName: "Si",
+        machineName: "si",
+        redirectUrl: "#.si"
+      }
+    ]
+  },
+  {
+    machineName: "sk",
+    label: "Slovakia",
+    languages: [
+      {
+        label: "Slovenský",
+        shortName: "Sk",
+        machineName: "sk",
+        redirectUrl: "#.sk"
+      }
+    ]
+  },
+  {
+    machineName: "za",
+    label: "South Africa",
+    languages: [
+      {
+        label: "English",
+        shortName: "En",
+        machineName: "en",
+        redirectUrl: "#.co.za"
+      }
+    ]
+  },
+  {
+    machineName: "es",
+    label: "Spain",
+    languages: [
+      {
+        label: "Español",
+        shortName: "Es",
+        machineName: "es",
+        redirectUrl: "#.es"
+      }
+    ]
+  },
+  {
+    machineName: "se",
+    label: "Sweden",
+    languages: [
+      {
+        label: "Svenska",
+        shortName: "Sv",
+        machineName: "sv",
+        redirectUrl: "#.se"
+      }
+    ]
+  },
+  {
+    machineName: "ch",
+    label: "Switzerland",
+    languages: [
+      {
+        label: "Deutsch",
+        shortName: "De",
+        machineName: "de",
+        redirectUrl: "#.ch"
+      },
+      {
+        label: "Français",
+        shortName: "Fr",
+        machineName: "fr",
+        redirectUrl: "#.ch/fr"
+      }
+    ]
+  },
+  {
+    machineName: "tw",
+    label: "Taiwan",
+    languages: [
+      {
+        label: "Taiwan",
+        shortName: "Tw",
+        machineName: "tw",
+        redirectUrl: "#.tw"
+      }
+    ]
+  },
+  {
+    machineName: "th",
+    label: "Thailand",
+    languages: [
+      {
+        label: "ภาษาไทย",
+        shortName: "Th",
+        machineName: "th",
+        redirectUrl: "#.co.th"
+      }
+    ]
+  },
+  {
+    machineName: "tn",
+    label: "Tunisia",
+    languages: [
+      {
+        label: "Français",
+        shortName: "Fr",
+        machineName: "fr",
+        redirectUrl: "#.tn"
+      }
+    ]
+  }
+  {
+    machineName: "tr",
+    label: "Turkey",
+    languages: [
+      {
+        label: "Türk",
+        shortName: "Tr",
+        machineName: "tr",
+        redirectUrl: "#.tr"
+      }
+    ]
+  },
+  {
+    machineName: "uk",
+    label: "United Kingdom",
+    languages: [
+      {
+        label: "English",
+        shortName: "En",
+        machineName: "en",
+        redirectUrl: "#.co.uk"
+      }
+    ]
+  },
+  {
+    machineName: "us",
+    label: "United States",
+    languages: [
+      {
+        label: "English",
+        shortName: "En",
+        machineName: "en",
+        redirectUrl: "#"
+      }
+    ]
+  },
+  {
+    machineName: "ua",
+    label: "Ukraine",
+    languages: [
+      {
+        label: "Ukrainian",
+        shortName: "Uk",
+        machineName: "uk",
+        redirectUrl: "#.ua"
+      }
+    ]
+  },
+  {
+    machineName: "vn",
+    label: "Vietnam",
+    languages: [
+      {
+        label: "Tiếng Việt",
+        shortName: "Vi",
+        machineName: "vi",
+        redirectUrl: "#.vn"
+      },
+      {
+        label: "English",
+        shortName: "En",
+        machineName: "en",
+        redirectUrl: "#.vn/en"
+      }
+    ]
+  },
+  {
+    machineName: "ve",
+    label: "Venezuela",
+    languages: [
+      {
+        label: "Español",
+        shortName: "Es",
+        machineName: "es",
+        redirectUrl: "#.es"
+      }
+    ]
+  }
+];
